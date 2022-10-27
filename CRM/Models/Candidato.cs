56e673a555ff5d95af4dcda8e710e00cd0de30fc
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-10-27 10:44:15 -0300

    :sparkles: feat: Criando model de candidato

---
+++ CRM/Models/Candidato.cs
@@ -0,0 +1,9 @@
+namespace CRM.Models;
+
+public class Candidato
+{
+  public int CandidatoId { get; set; }
+  public string CandidatoName { get; set; }
+  public string CandidatoEmail { get; set; }
+  public string CandidatoCpf { get; set; }
+}
